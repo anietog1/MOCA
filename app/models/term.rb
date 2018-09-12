43792dd569cb2ae8bb13bcc3527fb2c8046bc58f
Agustin Nieto <anietog1@eafit.edu.co>
2018-09-12 13:17:46 -0500

    Adding basic views for working with the students.

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,7 +1,7 @@
 class Term < ApplicationRecord
   has_many :advisors
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :start_date, presence: true
   validates :end_date, presence: true
 end
