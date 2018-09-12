afdf89a69015bb5532e498e44a89ba7460da26d1
evinracher <eevinley@gmail.com>
2018-09-12 13:36:28 -0500

    Adding views of terms, undergraduates and subjects

---
+++ app/views/undergraduates/_undergraduate.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! undergraduate, :id, :name, :created_at, :updated_at
+json.url undergraduate_url(undergraduate, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :undergraduates
+  resources :subjects
+  resources :terms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
