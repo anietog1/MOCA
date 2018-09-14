5219a0c32e1fb233edbb23da57af422d569c94f0
Agustin Nieto <anietog1@eafit.edu.co>
2018-09-14 11:37:53 -0500

    Adding the validation of advisors. It doesn't respond to json format, yet.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :applications, only: [:index]
+
+  namespace :applications do
+    resources :advisors, only: [:index, :show]
+    get 'advisors/accept/:id', to: 'advisors#accept', as: 'advisor_accept'
+    get 'advisors/reject/:id', to: 'advisors#reject', as: 'advisor_reject'
+  end
 end
