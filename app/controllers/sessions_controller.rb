930fcd94e4448579acec13ff8292ce79e5b4f5a2
evinracher <eevinley@gmail.com>
2018-09-19 19:48:23 -0500

    Try to create sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,11 +11,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # Get advisor data
+    @advisor = Advisor.find(params.permit(:id)[:id])
+    # Get student data
+    @student = Student.find(@advisor.student_id)
+    @sessions = @advisor.advisor_has_sessions
+    tag_ids = params.permit({ :tag_ids => [] })[:tag_ids]
+    tag_ids.each do |session|
+      if session == true
+        sessions.create(advisor_id: @advisor.id, day_has_hour_id: session )
+      end
+    end
   end
 
   def edit
   end
 
   def show_creating
+    @advisor = Advisor.find(params.permit(:id)[:id])
+    @days = Day.all
+    @hours = Hour.all
+    @sessions_show = DayHasHour.all
   end
 end
