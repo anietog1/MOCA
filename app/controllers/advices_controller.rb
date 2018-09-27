1512fcd4382fc2826d438b45e65697a32e7e2458
Agustin Nieto <anietog1@eafit.edu.co>
2018-09-26 21:52:26 -0500

    Editing advices' views and controller, trying them to solve the problem as they should. It's not working yet.

--- app/controllers/advices_controller.rb
+++ app/controllers/advices_controller.rb
@@ -1,21 +1,41 @@
 class AdvicesController < ApplicationController
+  def index
+    @sessions = Session.all
+  end
+
   def new
     @session = Session.new
   end
 
+  # UNFINISHED
   def create
-    @session = Session.new(session_params)
+    sessions = Session.where(session_params)
 
-    if @session.save
-      redirect_to @session
+    if sessions.any?
+      
     else
-      render 'new'
+      sessions = Session.where(session_params.except(:subject_id))
+
+      if sessions.any?
+
+      end
     end
   end
 
   private
   def session_params
-    params.require(:session)
-      .permit(:term_id, :day_id, :hour_id, :advisor_id, :student_id)
+    temp = params.require(:advice).permit(:term_id, :day_id, :hour_id, :subject_id)
+
+    {
+      term_id: temp[term_id],
+      day_has_hour_id: DayHasHour.find_by(day_id: temp[:day_id], hour_id: temp[:hour_id]),
+      subject_id: temp[term_id]
+    }
+  end
+
+  def student_params
+    temp = params.require(:advice).permit(:student_university_code)
+
+    { id: Student.find_by(university_code: temp[:student_university_code]).id }
   end
 end
