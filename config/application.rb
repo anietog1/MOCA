947beb1860dc3dadebc0e4416535f655757ef27b
Agustin Nieto <anietog1@eafit.edu.co>
2018-10-10 13:49:11 -0500

    Changing the location of the require csv.

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require csv here instead of inside each model
-require 'csv'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
