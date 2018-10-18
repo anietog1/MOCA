65590135d034870ffd3a7d8118396ef5ea3d283e
Agustin Nieto <anietog1@eafit.edu.co>
2018-10-18 09:56:53 -0500

    Adding devise gem and initializing it.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_06_030705) do
+ActiveRecord::Schema.define(version: 2018_10_18_144220) do
 
   create_table "advisor_has_subjects", force: :cascade do |t|
     t.integer "advisor_id", null: false
@@ -187,6 +187,21 @@ ActiveRecord::Schema.define(version: 2018_10_06_030705) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_undergraduates_on_name", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "kind", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["student_id"], name: "index_users_on_student_id"
   end
 
 end
