2c813b1a2d52620d4e55b5818765cc6a6747811e
Agustin Nieto <anietog1@eafit.edu.co>
2018-11-15 08:51:25 -0500

    Adding schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_144220) do
+ActiveRecord::Schema.define(version: 2018_11_14_142357) do
 
   create_table "advisor_has_subjects", force: :cascade do |t|
     t.integer "advisor_id", null: false
